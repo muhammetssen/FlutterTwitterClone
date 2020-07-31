474cc25366e3b95f2211072e18e76efd48f3be86
Muhammet Şen <enmuhammet23@gmail.com>
2020-07-31 23:53:53 +0300

    Date and like-rt count

---
+++ lib/Tweet/SingleTweetPage.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SingleTweetPage extends StatefulWidget {
+  var tweet;
+  SingleTweetPage({this.tweet});
+  @override
+  State<StatefulWidget> createState() {
+    // print(tweet);
+    return _SingleTweetPage(tweet);
+  }
+}
+
+class _SingleTweetPage extends State<SingleTweetPage> {
+  final blueColor = Color(0xff1BA1F3);
+  final backgroundColor = Color(0xff000000);
+  var tweet;
+  _SingleTweetPage(tweet) {
+    this.tweet = tweet;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(color: blueColor),
+        title: Text("Thread"),
+        backgroundColor: backgroundColor,
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(color: backgroundColor)),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: 80,
+                child: Row(children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage('assets/images/eggIcon.jpg')),
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('name'),
+                        Text(
+                          '@username',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  new Spacer(),
+                  IconButton(
+                    alignment: Alignment.center,
+                    icon: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {},
+                  )
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  tweet['text'],
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  tweet['sentTime'],
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+              ),
+              Container(
+                height: 25,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text('  ${tweet['retweetCount']}'),
+                    Text(
+                      ' Retweets         ',
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                    Text('${tweet['likeCount']}'),
+                    Text(
+                      ' Likes         ',
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Container(
+                  height: 40,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(FontAwesomeIcons.comment,
+                              color: Color(0xff49494B)),
+                          onPressed: () {}),
+                      IconButton(
+                          icon: Icon(FontAwesomeIcons.retweet,
+                              color: Color(0xff49494B)),
+                          onPressed: () {}),
+                      IconButton(
+                          icon: Icon(FontAwesomeIcons.heart,
+                              color: Color(0xff49494B)),
+                          onPressed: () {}),
+                      IconButton(
+                          icon: Icon(FontAwesomeIcons.shareAlt,
+                              color: Color(0xff49494B)),
+                          onPressed: () {}),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+              ),
+              // ListView(
+
+              // )
+
+              // Padding(
+              //   padding: const EdgeInsets.all(8.0),
+              //   child: Text(
+              //     '${tweet['retweetCount']} Retweets  ${tweet['likeCount']} Likes',
+              //     style: TextStyle(color: Colors.grey),),
+              // ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}

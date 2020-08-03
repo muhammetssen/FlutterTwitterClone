8d3bd76e373464de4656ad5820df813e8dc3952b
Muhammet Şen <enmuhammet23@gmail.com>
2020-08-03 22:55:43 +0300

    Added local and remote server IP addresses

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,5 @@
 library twitter_clone.globals;
 
-String ServerIPa = 'http://192.168.1.2:8080/';
-String ServerIP = "http://35.209.140.129:8082/";
+String localServerIP = 'http://192.168.1.2:8080/';
+String dockerServerIP = "http://35.209.140.129:8082/";
+String serverIP = localServerIP;
